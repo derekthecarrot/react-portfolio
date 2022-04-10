b6b83757070bf34b1d0c2e64103841946a497a2d
Jeffery <jpastvait@gmail.com>
2022-04-10 14:03:04 -0400

    swapping back to browserrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer'
 import NavBar from './components/NavBar'
 import Projects from './components/Projects'
